2d033ca5cb219470e328f8938cf25f0f31fe1121
PORSELVAN2003 <111323524+PORSELVAN2003@users.noreply.github.com>
2025-03-11 09:21:50 +0530

    Delete Angular Employee Management directory

--- Angular Employee Management/src/app/pipes/salary-formatter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SalaryFormatterPipe } from './salary-formatter.pipe';
-
-describe('SalaryFormatterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SalaryFormatterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
